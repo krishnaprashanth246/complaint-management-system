db5a854bc3e324afcfdff1b5cb201fc23b7a91da
Asish Pulikonda <asishmail123@gmail.com>
2022-04-29 23:43:58 +0530

    Added headings for each role description

--- src/components/enduser-description.component.js
+++ src/components/enduser-description.component.js
@@ -39,7 +39,7 @@ class Description extends Component{
       }
         return(
             <div style={styles}>
-                <h2 >Welcome !</h2>
+                <h2 >Welcome {JSON.parse(localStorage.getItem('loginData')).profileObj.name}!</h2>
                 <p >The purpose of this platform is to provide support to the end users who are facing any IT related issues on IITH campus. End users can raise a ticket for their complaint and a technician will be assigned to solve the problem.
                 </p>
 
