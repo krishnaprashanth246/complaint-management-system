096214945ec693018ea0fe901341f753163c4a98
Vishal <gvishalsiva@gmail.com>
2022-04-29 15:31:22 +0530

    Updated enduser closed component

--- src/components/enduser-closed.component.js
+++ src/components/enduser-closed.component.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
-import { Link , Redirect} from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import EndUserSidebar from './enduser-sidebar.component';
+import EndUserTicketPrinter from './enduser-ticket-printer.component';
+import axios from 'axios';
 
 class EndUserClosed extends Component{
     
+    constructor(props) {
+		super(props);
+
+		// this.deleteTicket = this.deleteTicket.bind(this);
+
+		this.state = { tickets: [] };
+	}
+    
+    componentDidMount() {
+        axios.get(`http://localhost:5000/tickets/email/${JSON.parse(localStorage.getItem('loginData')).profileObj.email}`
+            // , 
+            // {
+            // params: {
+            //   email: JSON.parse(localStorage.getItem('loginData')).profileObj.email
+            // }}
+            )
+            .then(res => {
+                this.setState({ tickets: res.data })
+            })
+            .catch(error => console.log(error));
+    }
+
+    getClosedList() {
+        return this.state.tickets.map(currentTicket => {
+            if(currentTicket.ticketStatus === "Closed") 
+                return <EndUserTicketPrinter 
+            			ticket={currentTicket} 
+                        key={currentTicket._id}
+                        />;
+        })
+	}
+
     render()
     {
         if(localStorage.getItem("loginData") == null){
@@ -15,13 +49,36 @@ class EndUserClosed extends Component{
         }
 
         return(
+            // <div>
             <div className='wrapper'>
                  <EndUserSidebar />
-                <div>
-                    <h1>Your Closed Tickets</h1>
-                </div>
+            <div>
+                <br></br>
+				<h3>Closed Tickets</h3>
+                    <table className="table">
+                        <thead className="thead-light">
+                        <tr>
+                            <th>Ticket Info</th>
+                            <th>Category Name</th>
+                            <th>Assigned Technician</th>
+                            <th>Opened Date</th>
+                            <th>Last Updated</th>
+                            <th>Status</th>
+                            <th>Feedback</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            { this.getClosedList() }
+                        </tbody>
+                    </table>
+			</div>
+
             </div>
-        );
+
+            
+            
+        );      
     };
 
 }
